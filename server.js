2900fbdeb5f81df4b2e873b32fd92452f38cf017
gerard.rique <gerard.rique@my.uwi.edu>
2018-11-13 13:50:06 -0400

    wrote code for node server and moved files as necassary

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express')
+
+let app = express();
+
+let path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/home.html'));
+})
+
+app.get('/sign-up', (req, res) => {
+    res.sendFile(path.join(__dirname + '/register.html'));
+})
+
+app.get('/log-in', (req, res) => {
+    res.sendFile(path.join(__dirname + '/login.html'));
+})
+
+app.get('/pricing', (req, res) => {
+    res.sendFile(path.join(__dirname + '/pricing.html'));
+})
+
+app.listen(8080);
