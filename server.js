41658e4dc86737ff742411ea18ae7436f24513d4
Kerschel James <kerschels@hotmail.com>
2018-11-18 18:21:30 -0400

    Finished insert statements and profile history

--- server.js
+++ server.js
@@ -8,8 +8,13 @@ var connection = mysql.createConnection({
     host:'tvdrzajhtt79sf.cnuipuiqebbi.us-east-1.rds.amazonaws.com',
     user:'admin',
     password:'password123',
-    database:'appDB'
+    database:'appDB',
+    multipleStatements: true
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
+var urlencodedParser = bodyParser.urlencoded({extended:false})
+
 connection.connect(function(err) {
     if (err) {
       console.error('Database connection failed');
@@ -19,72 +24,62 @@ connection.connect(function(err) {
     console.log('Connected to database.');
   });
 
-  function createTables(){
-    // var sql = "CREATE TABLE users (ID INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255),password VARCHAR (100),accounttype VARCHAR (20) ,PRIMARy KEY(ID)) ";
-//    var id =1;
-//    var first_name ='kj';
-//     var sql = "INSERT INTO users VALUES(id,first_name,'James','kerschels@hotmail.com','testing123','teacher')";
-    
-    //     connection.query(sql, function (err, result) {
-    //       if (err) throw err;
-    //       console.log("User Table Created");
-    // })
-    
-     var sqls = "CREATE TABLE plans (id INT NOT NULL AUTO_INCREMENT, type VARCHAR(255) NOT NULL, active varchar(1),customerid int not null,  PRIMARy KEY(ID) , FOREIGN KEY (customerid) REFERENCES users (ID)) ";
-     connection.query(sqls, function (err, result) {
-        if (err) throw err;
-        console.log("Plans Table Created");
+  function createUserTable(){
+    var sql = "CREATE TABLE users (ID INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255),password VARCHAR (100),accounttype VARCHAR (20) ,PRIMARy KEY(ID)) ";
+
+        connection.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("User Table Created");
     })
+    
+    
+  }
+
+  function createPlanTable(){
+    var sqls = "CREATE TABLE plans (id INT NOT NULL AUTO_INCREMENT, type VARCHAR(255) NOT NULL, active varchar(1),customerid int not null,  PRIMARy KEY(ID) , FOREIGN KEY (customerid) REFERENCES users (ID)) ";
+    connection.query(sqls, function (err, result) {
+       if (err) throw err;
+       console.log("Plans Table Created");
+   })
   }
 
 
   function dropTables(){
-      var drop = "drop table users ";
-      var dropplans = "drop table plans";
+      var drop = "drop table users;drop table plans";
 
-
-    //   connection.query(drop, function (err, result) {
-    //      if (err) throw err;
-    //      console.log("Plans Table Dropped");
-    //  })
-     connection.query(dropplans, function (err, result) {
-        if (err) throw err;
-        console.log("Plans Table dropped");
-    })
+      connection.query(drop, function (err, result) {
+         if (err) throw err;
+         console.log("Plans Table Dropped");
+     })
+   
   }
 
-//   var sqls = "INSERT into plans VALUES (1,'free','1',1)";
-//   var sqls = "select *";
 
-//      connection.query(sqls, function (err, result) {
-//         if (err) throw err;
-//         console.log(result);
-//     })
-// dropTables();
-// createTables();
-app.use(express.static(path.join(__dirname, 'public')));
-var urlencodedParser = bodyParser.urlencoded({extended:false})
+
+
 
 app.post('/saveuser',urlencodedParser   , (req, res) => {
     let body = req.body;
-    var  first_name = body.firstname;
-    var  last_name = body.lastname;
-    var  password = body.pass;
-    var  accounttype = body.type;
-    var  email = body.email;
+    var  first_names = body.firstname;
+    var  last_names = body.lastname;
+    var  passwords = body.pass;
+    var  accounttypes = body.type;
+    var  emails = body.email;
 
 
-    
-    var sql = "INSERT INTO users VALUES(15,"+first_name+","+last_name+","+email+","+password+","+accounttype+")";
-        // var sql = "INSERT INTO users VALUES(1,'Kerschel','James','kerschels@hotmail.com','testing123','teacher')";
+    var values = {first_name:first_names,
+                    last_name:last_names,
+                    password:passwords,
+                    email:emails,
+                    accounttype:accounttypes
+                }
 
-    connection.query(sql ,function (err, result) {
+    var sql = "INSERT INTO users SET ?";
+    connection.query(sql ,values,function (err, result) {
     if (err) throw err;
     console.log(result,err);
     })
-        
     res.sendFile(path.join(__dirname + '/login.html'));
-
 })
 var account = null;
 var accountid = null;
@@ -92,9 +87,12 @@ app.post('/getloggedin',urlencodedParser, (req, res) => {
     var body = req.body;
     var email = body.email;
     var password = body.pass;
-    var cred = 'SELECT id,password from users where email = email';
-    connection.query(cred, function (err, result) {
+    // console.log(body);
+
+    var cred = 'SELECT id,password from users where email = ?';
+    connection.query(cred, email,function (err, result) {
         if (err) throw err;
+        // console.log(result);
         if (result[0]['password'] == password){
             accountid =result[0]['id'];
             account = email;
@@ -114,6 +112,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/sign-up', (req, res) => {
+    if(accountid !=null){
+    res.sendFile(path.join(__dirname + '/profile.html'));
+
+    }
+    else
     res.sendFile(path.join(__dirname + '/register.html'));
 })
 
@@ -130,7 +133,6 @@ app.get('/profile', (req, res) => {
     
       res.sendFile(path.join(__dirname + '/profile.html'));
 })
-
 app.get('/profile/get',(req,res)=>{
     console.log(account);
     var cred = `SELECT * from users where users.id = `+accountid+` `;
@@ -141,19 +143,50 @@ app.get('/profile/get',(req,res)=>{
 });
 
 app.get('/profile/plan',(req,res)=>{
-    var cred = 'SELECT * from plans where plans.customerid in (SELECT id FROM users where id = '+accountid+')';
-    connection.query(cred, function (err, result) {
+    var cred = 'SELECT * from plans where plans.customerid = ?';
+    connection.query(cred, accountid,function (err, result) {
+        // console.log(result)
     res.send((result));
 })
 });
 
 
+app.post('/profile/add/free',(req,res)=>{
+    var values =
+    {
+        type:'free',
+        active:'1',
+        customerid:accountid
+    }
+    var insert = 'INSERT INTO plans SET ? ';
+    connection.query(insert, values,function (err, result) {
+    res.sendFile(path.join(__dirname + '/home.html'));
+})
+});
 
-app.get('/profile/add/free',(req,res)=>{
-    // var email = 'kerschels@hotmail.com'
-    // var insert = 'INSERT INTO plans ';
-    connection.query(cred, function (err, result) {
+app.post('/profile/add/basic',(req,res)=>{
+    var values =
+    {
+        type:'basic',
+        active:'1',
+        customerid:accountid
+    }
+    var insert = 'INSERT INTO plans SET ? ';
+    connection.query(insert, values,function (err, result) {
      
+    res.send((result));
+})
+});
+
+app.post('/profile/add/enterprise',(req,res)=>{
+    var values =
+    {
+        type:'enterprise',
+        active:'1',
+        customerid:accountid
+    }
+    var insert = 'INSERT INTO plans SET ? ';
+    connection.query(insert, values,function (err, result) {
     res.send((result));
 })
 });
