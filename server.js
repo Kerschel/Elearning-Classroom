063361c02df5d1c0eb7ed3c98b8a0a2d417a4edf
Amar Andrew Singh <princeandrew01@gmail.com>
2018-11-20 18:41:58 -0400

    Commit docker changes and updated the readme with instructions.

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+var prefix = "aas_";
+var PORT = 8080;
+var CONFIG_TABLENAME = prefix + "config";
+var USERS_TABLE = prefix + "users";
+var PLANS_TABLE = prefix + "plans";
+
 var express = require('express')
 let app = express();
 let path = require('path');
@@ -5,10 +11,10 @@ var mysql = require('mysql');
 
 var bodyParser = require('body-parser');
 var connection = mysql.createConnection({
-    host:'tvdrzajhtt79sf.cnuipuiqebbi.us-east-1.rds.amazonaws.com',
-    user:'admin',
-    password:'password123',
-    database:'appDB',
+    host: process.env.DATABASE_HOST,
+    user:process.env.MYSQL_USER,
+    password:process.env.MYSQL_PASSWORD,
+    database:process.env.MYSQL_DATABASE,
     multipleStatements: true
 });
 
@@ -20,39 +26,51 @@ connection.connect(function(err) {
       console.error('Database connection failed');
       return;
     }
-  
     console.log('Connected to database.');
+    /* First time run */
+    //var sql = "select count(*) as exists from information_schema.tables where table_schema='" + process.env.DATABSE_HOST + "'";
+    var sql = "select count(*) as cnt from information_schema.tables where table_schema='" + process.env.DATABASE_HOST + "' and table_name ='" + CONFIG_TABLENAME + "'";
+    connection.query(sql, function (err, result) {
+      if (err) {
+        console.error('Initial Load failed');
+        return;
+      }
+      console.log(result);
+      if (result[0].cnt == 0 ) {
+        var sql = "CREATE TABLE " + CONFIG_TABLENAME + "(ID INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, value text not null, PRIMARY KEY(ID))";
+        connection.query(sql, function (err, result) {
+          if (err) {
+            return;
+          }
+          console.log("Config table created");
+        });
+        var sql = "INSERT INTO " + CONFIG_TABLENAME + "(ID, name, value) values (null, 'setup_complete', '1')";
+        connection.query(sql, function (err, result) {
+          if (err) {
+            return;
+          }
+          console.log("Config table populated");
+        });
+
+        var sql = "CREATE TABLE " + USERS_TABLE + " (ID INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255),password VARCHAR (100),accounttype VARCHAR (20) ,PRIMARY KEY(ID)) ";
+        connection.query(sql, function (err, result) {
+          if (err) {
+            return;
+          }
+          console.log("Users table created");
+        });
+
+        var sql = "CREATE TABLE " + PLANS_TABLE + " (id INT NOT NULL AUTO_INCREMENT, type VARCHAR(255) NOT NULL, active varchar(1),customerid int not null,  PRIMARy KEY(ID) , FOREIGN KEY (customerid) REFERENCES users (ID)) ";
+        connection.query(sql, function (err, result) {
+          if (err) {
+            return;
+          }
+          console.log("Plan table created");
+        });
+      }
+    });
   });
 
-  function createUserTable(){
-    var sql = "CREATE TABLE users (ID INT NOT NULL AUTO_INCREMENT, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255),password VARCHAR (100),accounttype VARCHAR (20) ,PRIMARy KEY(ID)) ";
-
-        connection.query(sql, function (err, result) {
-          if (err) throw err;
-          console.log("User Table Created");
-    })
-    
-    
-  }
-
-  function createPlanTable(){
-    var sqls = "CREATE TABLE plans (id INT NOT NULL AUTO_INCREMENT, type VARCHAR(255) NOT NULL, active varchar(1),customerid int not null,  PRIMARy KEY(ID) , FOREIGN KEY (customerid) REFERENCES users (ID)) ";
-    connection.query(sqls, function (err, result) {
-       if (err) throw err;
-       console.log("Plans Table Created");
-   })
-  }
-
-
-  function dropTables(){
-      var drop = "drop table users;drop table plans";
-
-      connection.query(drop, function (err, result) {
-         if (err) throw err;
-         console.log("Plans Table Dropped");
-     })
-   
-  }
 
 
 
@@ -74,7 +92,7 @@ app.post('/saveuser',urlencodedParser   , (req, res) => {
                     accounttype:accounttypes
                 }
 
-    var sql = "INSERT INTO users SET ?";
+    var sql = "INSERT INTO " + USERS_TABLE + " SET ?";
     connection.query(sql ,values,function (err, result) {
     if (err) throw err;
     console.log(result,err);
@@ -89,7 +107,7 @@ app.post('/getloggedin',urlencodedParser, (req, res) => {
     var password = body.pass;
     // console.log(body);
 
-    var cred = 'SELECT id,password from users where email = ?';
+    var cred = 'SELECT id,password from ' + USERS_TABLE + ' where email = ?';
     connection.query(cred, email,function (err, result) {
         if (err) throw err;
         // console.log(result);
@@ -130,12 +148,12 @@ app.get('/pricing', (req, res) => {
 })
 
 app.get('/profile', (req, res) => {
-    
+
       res.sendFile(path.join(__dirname + '/profile.html'));
 })
 app.get('/profile/get',(req,res)=>{
     console.log(account);
-    var cred = `SELECT * from users where users.id = `+accountid+` `;
+    var cred = `SELECT * from ` + USERS_TABLE  +` where users.id = `+accountid+` `;
     connection.query(cred, function (err, result) {
      console.log(result,err);
     res.send((result));
@@ -143,7 +161,7 @@ app.get('/profile/get',(req,res)=>{
 });
 
 app.get('/profile/plan',(req,res)=>{
-    var cred = 'SELECT * from plans where plans.customerid = ?';
+    var cred = 'SELECT * from ' + PLANS_TABLE + ' where plans.customerid = ?';
     connection.query(cred, accountid,function (err, result) {
         // console.log(result)
     res.send((result));
@@ -158,7 +176,7 @@ app.post('/profile/add/free',(req,res)=>{
         active:'1',
         customerid:accountid
     }
-    var insert = 'INSERT INTO plans SET ? ';
+    var insert = 'INSERT INTO ' + PLANS_TABLE + ' SET ? ';
     connection.query(insert, values,function (err, result) {
     res.sendFile(path.join(__dirname + '/home.html'));
 })
@@ -171,9 +189,9 @@ app.post('/profile/add/basic',(req,res)=>{
         active:'1',
         customerid:accountid
     }
-    var insert = 'INSERT INTO plans SET ? ';
+    var insert = 'INSERT INTO ' + PLANS_TABLE + ' SET ? ';
     connection.query(insert, values,function (err, result) {
-     
+
     res.send((result));
 })
 });
@@ -185,7 +203,7 @@ app.post('/profile/add/enterprise',(req,res)=>{
         active:'1',
         customerid:accountid
     }
-    var insert = 'INSERT INTO plans SET ? ';
+    var insert = 'INSERT INTO ' + PLANS_TABLE + ' SET ? ';
     connection.query(insert, values,function (err, result) {
     res.send((result));
 })
@@ -194,4 +212,4 @@ app.post('/profile/add/enterprise',(req,res)=>{
 
 
 
-app.listen(8080);
+app.listen(PORT);
